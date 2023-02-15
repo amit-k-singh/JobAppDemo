a19473bb54627a189293b5e22d8861f490154b3a
Shubham Desai <desaishubham82@gmail.com>
2023-02-15 10:42:30 +0530

    comment in COre infra

--- JobAppDemo.Core.Contract/IDepartmentServices.cs
+++ JobAppDemo.Core.Contract/IDepartmentServices.cs
@@ -10,7 +10,7 @@ namespace JobAppDemo.Core.Contract
         Task <List<DepartmentResponseModel>> GetDepartmentsAsync();
         Task<DepartmentResponseModel> GetDepartmentByIdAsync(int id);
 
-        //ShubhamDesai
+        //ShubhamDesaiiiiiii
 
         Task AddDepartmentAsync(DepartmentRequestModel department);
 
